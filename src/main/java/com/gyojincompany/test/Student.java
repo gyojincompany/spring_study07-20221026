030c0d15c67b433b0ba51d4665a67012fca77a53
gyojincompany <luckyred@gmail.com>
2022-10-26 12:35:04 +0900

    컨테이너 생명주기 활용 연습

---
+++ src/main/java/com/gyojincompany/test/Student.java
@@ -0,0 +1,54 @@
+package com.gyojincompany.test;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Student implements InitializingBean, DisposableBean {
+	
+	private String name;
+	private int age;
+	
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 호출!!");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("afterPropertiesSet 호출!!");
+		
+	}
+	
+	
+
+}
